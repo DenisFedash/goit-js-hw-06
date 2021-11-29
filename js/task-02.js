7ca97cbbcc7522b47cd2489296ac62a027870d86
Denis Fedash <0501054104d@gmail.com>
2021-11-29 22:22:17 +0200

    Start hw

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,36 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const firstItem = document.createElement('li');
+firstItem.classList.add('item');
+firstItem.textContent = 'Potatoes';
+console.log(firstItem);
+
+const secondItem = document.createElement('li');
+secondItem.classList.add('item');
+secondItem.textContent = 'Mushrooms';
+console.log(secondItem);
+
+const thirdItem = document.createElement('li');
+thirdItem.classList.add('item');
+thirdItem.textContent = 'Garlic';
+console.log(thirdItem);
+
+const fourthItem = document.createElement('li');
+fourthItem.classList.add('item');
+fourthItem.textContent = 'Tomatos';
+console.log(fourthItem);
+
+const fifthItem = document.createElement('li');
+fifthItem.classList.add('item');
+fifthItem.textContent = 'Herbs';
+console.log(fifthItem);
+
+const sixthItem = document.createElement('li');
+sixthItem.classList.add('item');
+sixthItem.textContent = 'Condiments';
+console.log(sixthItem);
+
+const list = document.querySelector('#ingredients');
+list.append(firstItem, secondItem, thirdItem, fourthItem, fifthItem, sixthItem);
