6013aa6c654a8fee20c4c14eb7867e208f4eced9
Denis Fedash <0501054104d@gmail.com>
2021-12-01 16:43:50 +0200

    1, 2 tasks done

--- js/task-01.js
+++ js/task-01.js
@@ -2,11 +2,11 @@ const categoriesEl = document.querySelectorAll('.item');
 console.log('Number of categories: ', categoriesEl.length);
 
 const listEl = document.querySelector('#categories').children;
-for (let i = 0; i < listEl.length; i += 1) {
-    let category = listEl[i].firstElementChild.textContent;   
-    let quantityElem = listEl[i].lastElementChild.children.length;
-    console.log(`Category: ${category} \nElements: ${quantityElem}`);
-};
+for (const list of listEl) {
+    let category = list.firstElementChild.textContent;   
+    let quantityElem = list.lastElementChild.children.length;
+    console.log(`Category: ${category} \nElements: ${quantityElem}`);   
+}
 
 
 
