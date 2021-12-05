811931eed874415ca5f3c3e7407e883f8f3d568c
Denis Fedash <0501054104d@gmail.com>
2021-12-06 01:13:18 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener("submit", onSubmit);
+function onSubmit(event) {
+    event.preventDefault();
+    const { elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        return console.log("Все поля должны быть заполнены!");
+    }
+    
+    const formData = {
+        email: `${ email.value }`,
+        password: `${ password.value }`,
+    };
+    console.log(formData);
+    
+    event.currentTarget.reset()
+};
+
 
