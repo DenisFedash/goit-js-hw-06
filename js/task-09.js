4205ce978e16338c12037250e915b8ae5cba2d4d
Denis Fedash <0501054104d@gmail.com>
2021-12-06 02:38:38 +0200

    делаю 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  divEl: document.querySelector('.widget'),
+  spanEl: document.querySelector('.color'),
+  btnEl: document.querySelector('.change-color'),
+};
+
+refs.btnEl.addEventListener("click", function (e) {
+  document.body.style.backgroundColor = [`#${Math.floor(Math.random() * 16777215).toString(16)}`];
+  refs.spanEl.textContent = e.getRandomHexColor;
+});
+
+
+
+
+
